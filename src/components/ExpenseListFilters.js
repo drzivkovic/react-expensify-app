4e93bea9a383279a7a473114f3ab6c10f9042301
dr.zivkovic <dr.zivkovic@gmail.com>
2020-09-26 17:50:23 +0200

    Setup formating for amount and createdAt

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -49,6 +49,8 @@ export class ExpenseListFilters extends React.Component {
                     showClearDates={true}
                     numberOfMonths={1}
                     isOutsideRange={() => false}
+                    endDateId="asd"
+                    startDateId="dsa"
                 />
             </div>
         );
